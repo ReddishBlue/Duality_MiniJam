524313dd657e1e1f5714337a1db05436c7bad221
t. starynski <toristarynski@gmail.com>
2025-06-20 23:09:58 -0400

    bullet bounces!

--- DualityJam/Assets/teddy/PlayerBullet.cs
+++ DualityJam/Assets/teddy/PlayerBullet.cs
@@ -2,14 +2,48 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
-    private int collision count = 0;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private int bulletHP = 5;
+    public Rigidbody2D bulletRB;
+    public float speed = 20f;
+
+    private Vector2 direction;
+
+    //public bool isCollided = false; //for testing
+
+    public void Shoot(Vector2 direction){
+        this.direction = direction;
+        bulletRB.linearVelocity = this.direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (collisionCount >= 5) {
-            Destroy(gameObject);
+        bulletHP--; 
+
+        if (other.gameObject.tag == "Player") //TODO: fix tag as needed
+        {
+            Destroy(this.gameObject);
         }
-        else {
-            collision++;
-        }
+        else if (bulletHP < 0) {
+            Destroy(this.gameObject);
+            return;
+        } 
+
+        Vector2 newVelocity = Vector2.Reflect(direction.normalized, other.contacts[0].normal);
+        Shoot(newVelocity.normalized);
+
+        //isCollided = true; //for testing
+    
+    }
+
+    private void Update()
+    {
+        
+        //just to test it out
+        // if(!isCollided){
+        //     Vector2 dir = new Vector2(1f, 0.0f);
+        //     Shoot(dir.normalized);
+        // }
+        
+       
     }
 }
