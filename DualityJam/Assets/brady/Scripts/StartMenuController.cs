12b3ab38d31fd6f947116ccb1c989c7643191668
Brady <bradybock11@icloud.com>
2025-06-22 01:24:18 -0400

    added a start menu

---
+++ DualityJam/Assets/brady/Scripts/StartMenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartMenuController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void onStartClick()
+    {
+        SceneManager.LoadScene("Brady");
+    }
+
+    public void onExitClick()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
